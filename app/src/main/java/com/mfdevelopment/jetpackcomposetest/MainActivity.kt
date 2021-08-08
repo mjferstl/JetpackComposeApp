d844998922f78d4db86df63f50641899d26855c6
mjferstl <39524509+mjferstl@users.noreply.github.com>
2021-08-08 22:34:03 +0200

    Refactoring theme name

--- app/src/main/java/com/mfdevelopment/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/mfdevelopment/jetpackcomposetest/MainActivity.kt
@@ -8,13 +8,13 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.mfdevelopment.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
+import com.mfdevelopment.jetpackcomposetest.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTestTheme {
+            JetpackComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -32,7 +32,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeTestTheme {
+    JetpackComposeAppTheme {
         Greeting("Android")
     }
 }
